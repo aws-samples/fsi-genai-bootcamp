65fb6aa6a0353f93dab12ad53bb7afdb1c7ebb80
szamarin <szamarin@gmail.com>
2025-04-11 15:31:07 +0000

    rename directories

--- 11_langgraph_mcp_smai/servers/risk_assessor/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from servers.risk_assessor.model import RiskAssessorInput, RiskAssessorOutput
-from servers.risk_assessor.utils import assess_risk
-
-app = FastAPI()
-
-@app.post("/process", response_model=RiskAssessorOutput)
-async def process_risk(input_data: RiskAssessorInput):
-    return assess_risk(input_data)
